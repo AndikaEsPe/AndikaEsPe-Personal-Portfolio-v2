d699ecd139f1a8327018fd23c52f820d78b21341
Andika Sentosa Putra <andika@Andikas-MacBook-Pro.local>
2022-09-01 01:24:27 -0700

    Design About Me

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+var typed = new Typed(".typing", {
+    strings: ["Hello!", "Bonjour!","안녕하세요!", "Halo!", "Guten Tag!", "ハロ!", "Hola!", "您好!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
